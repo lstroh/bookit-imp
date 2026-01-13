7a8198006c4543f9d79d24108929835bcd13c1be
Liron Stroh <liron.stroh@gmail.com>
2026-01-13 09:17:39 +0000

    PRE-01

---
+++ wp-booking-plugin/booking-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Booking Plugin
+ * Plugin URI: https://example.com/booking-plugin
+ * Description: A WordPress booking plugin
+ * Version: 1.0.0
+ * Author: Your Name
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
